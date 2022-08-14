b56f6d37e45a0e307deabcac29f59eb171335483
颜龙 <yanlong@wx.bc>
2022-08-14 17:29:30 +0800

    删除失败用例

--- src/test/java/com/yl/junit5/testdemo/TestRunner.java
+++ src/test/java/com/yl/junit5/testdemo/TestRunner.java
@@ -1,12 +1,13 @@
-//package com.yl.junit5.testdemo;
-//
-//import com.yl.junit5.testdemo.addition.additionTest;
-//import org.junit.platform.runner.JUnitPlatform;
-//import org.junit.platform.suite.api.SelectClasses;
-//import org.junit.runner.RunWith;
-//
-//@RunWith(JUnitPlatform.class)
-//@SelectClasses({additionTest.class,})
-//public class TestRunner {
-//
-//}
+package com.yl.junit5.testdemo;
+
+import com.yl.junit5.testdemo.addition.additionTest;
+import com.yl.junit5.testdemo.subtraction.subtractionTest;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectClasses({additionTest.class, subtractionTest.class})
+public class TestRunner {
+
+}
